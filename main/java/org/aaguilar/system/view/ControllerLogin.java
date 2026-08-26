406f865648f8c382146787347bcd9c1a66f253cb
Adrii <aaguilar-2023146@kinal.edu.gt>
2026-08-26 08:15:33 -0600

    feat(Controladores): Creación de clases controladores para los archivos FXML.

---
+++ main/java/org/aaguilar/system/view/ControllerLogin.java
@@ -0,0 +1,4 @@
+package org.aaguilar.system.view;
+
+public class ControllerLogin {
+}
---
+++ main/java/org/aaguilar/system/view/ControllerRegistro.java
@@ -0,0 +1,7 @@
+package org.aaguilar.system.view;
+
+public class ControllerRegistro {
+
+    @FXML
+    private TextField textFieldNombre;
+}
